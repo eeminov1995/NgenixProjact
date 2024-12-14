7273be677296b1e49a7962c798cb73c4acaf0571
e.eminov <e.eminov@ngenix.net>
2024-12-14 19:29:14 +0300

    [add] demo project 3

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -8,12 +8,22 @@ def index():
 
 @app.route('/contacts')
 def contacts():
-    return render_template('contacts.html')
+    github_json_url = url_for('static', filename='github.json')
+    telegram_json_url = url_for('static', filename='tg.json')
+    vk_json_url = url_for('static', filename='vk.json')
+    return render_template('contacts.html',
+                           github_json_url=github_json_url,
+                           telegram_json_url=telegram_json_url,
+                           vk_json_url=vk_json_url)
 
 @app.route('/streams')
 def streams():
     return render_template('streams.html')
 
+@app.route('/services')
+def services():
+    return render_template('services.html')
+    
 @app.route('/videos')
 def videos():
     return render_template('videos.html')
@@ -24,3 +34,4 @@ def competencies():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
